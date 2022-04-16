8cdc5bafe9e6ba1b6d52b99109add2cb70c90a71
neshanth <59483685+neshanth@users.noreply.github.com>
2022-04-16 18:19:52 +0530

    added footer

--- resources/views/Layout/footer.blade.php
+++ resources/views/Layout/footer.blade.php
@@ -1,4 +1,37 @@
-<!-- JavaScript Bundle with Popper -->
+<footer>
+    <div class="site-footer py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-4">
+                    <p>CONTACT US</p>
+                </div>
+                <div class="col-4">
+                    <p>CONNECT WITH US</p>
+                </div>
+                <div class="col-4">
+                    <p>SUBSCRIBE</p>
+                    <form class="d-flex align-items-baseline">
+                        <div class="mb-3">
+                            <input type="email" placeholder="Email Address" class="form-control newsletter-input newsletter-input-footer" id="subscribe" aria-describedby="emailHelp">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Join</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="number">
+        <p>REGISTERED NUMBER : 1234567890</p>
+    </div>
+    <div class="copyright">
+        <p>&copy; <script>
+                document.write(/\d{4}/.exec(Date())[0])
+            </script>
+            TOPAZ
+        </p>
+    </div>
+</footer>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
 </body>
