94ef5d187aa671ca6107b1a6d7af85da2d9f2f2d
neshanth <59483685+neshanth@users.noreply.github.com>
2022-05-01 18:44:19 +0530

    fixed admin hamburger color

--- resources/views/Dashboard/navbar.blade.php
+++ resources/views/Dashboard/navbar.blade.php
@@ -3,7 +3,7 @@
     <!-- Left navbar links -->
     <ul class="navbar-nav">
         <li class="nav-item">
-            <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+            <a class="nav-link" style="color:#244150" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
         </li>
     </ul>
     <!-- Right navbar links -->
