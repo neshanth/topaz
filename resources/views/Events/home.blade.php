a734e8461880333374514264f17e24982d7e3c4e
neshanth <59483685+neshanth@users.noreply.github.com>
2022-05-07 11:16:44 +0530

    changed events to use cards

--- resources/views/Events/home.blade.php
+++ resources/views/Events/home.blade.php
@@ -5,23 +5,24 @@
     <div class="row px-3 my-3">
         <h2 class="text-center py-3">Our Events</h2>
         @foreach ($events as $event)
-        <div class="col-md-3 my-3">
-            <div class="event">
-                <img src="{{ asset("storage/event/".$event->event_image) }}" class="d-block w-100" />
-                <div class="event-info">
-                    <p class="event-cat">{{ $event->category }}</p>
-                    <h4 class="event-title">{{ $event->event_name }}</h4>
-                    <p class="event-donation">${{ $event->donation_received}} Donated of ${{ $event->total_donation }}</p>
-                    <div class="progress">
-                        <div class="progress-bar" style="width:<?= ceil(($event->donation_received / $event->total_donation) * 100) ?>%" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
+        <a href="{{ route("events.show",[$event->id]) }}">
+            <div class="col-md-3 my-3">
+                <div class="event card">
+                    <img src="{{ asset("storage/event/".$event->event_image) }}" class="d-block w-100 card-img-top" />
+                    <div class="event-info card-body">
+                        <p class="event-cat">{{ $event->category }}</p>
+                        <h4 class="event-title">{{ $event->event_name }}</h4>
+                        <p class="event-donation">${{ $event->donation_received}} Donated of ${{ $event->total_donation }}</p>
+                        <div class="progress">
+                            <div class="progress-bar" style="width:<?= ceil(($event->donation_received / $event->total_donation) * 100) ?>%" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                    </div>
+                    <div class="event-details card-footer">
+                        {!! substr(strip_tags($event->event_desc),0, 100) !!}...
                     </div>
                 </div>
-                <div class="event-details">
-                    {!! substr(strip_tags($event->event_desc),0, 100) !!}
-                    <a href="{{ route("events.show",[$event->id]) }}">Read more</a>
-                </div>
             </div>
-        </div>
+        </a>
         @endforeach
     </div>
 
--- resources/views/Events/show.blade.php
+++ resources/views/Events/show.blade.php
@@ -5,7 +5,7 @@
 
 <div class="container">
     <div class="row py-3 px-2">
-        <h2 class="text-center my-3">{{ $event->event_name }}</h2>
+        <h2 class="text-center my-3 single-event-title">{{ $event->event_name }}</h2>
         <p class="text-center text-custom-primary" style="font-size: 1.4rem;">Donation Raised</p>
         <div class="progress single-event-progress">
             <div class="progress-bar single-event-bar" style="width:<?= ceil(($event->donation_received / $event->total_donation) * 100) ?>%" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
